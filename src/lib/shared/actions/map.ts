a0335bae7c16c7ad7fc129ff2cb6b4c962c18cd7
Rodney Lab <codesign@rodneylab.com>
2023-01-05 18:29:54 +0000

    fix: 💫 add interactive map example

--- src/lib/shared/actions/map.ts
+++ src/lib/shared/actions/map.ts
@@ -2,59 +2,68 @@ import markerIconRetinaURL from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIconURL from 'leaflet/dist/images/marker-icon.png';
 import markerShadowURL from 'leaflet/dist/images/marker-shadow.png';
 import 'leaflet/dist/leaflet.css';
+import type { Map } from 'leaflet';
 
-export function setMap(
+interface MapNodeProps {
+	latitude: number;
+	longitude: number;
+	zoom: number;
+	mapboxAccessToken: string;
+	markerMarkup?: string;
+}
+
+async function updateMap(
 	mapElement: HTMLElement,
-	{
-		latitude,
-		longitude,
-		zoom,
-		mapboxAccessToken,
-		markerMarkup = ''
-	}: {
-		latitude: number;
-		longitude: number;
-		zoom: number;
-		mapboxAccessToken: string;
-		markerMarkup?: string;
-	}
+	{ latitude, longitude, zoom, mapboxAccessToken, markerMarkup = '' }: MapNodeProps
 ) {
-	(async () => {
-		const {
-			icon: leafletIcon,
-			map: leafletMap,
-			marker: leafletMarker,
-			tileLayer
-		} = await import('leaflet');
+	const {
+		icon: leafletIcon,
+		map: leafletMap,
+		marker: leafletMarker,
+		tileLayer
+	} = await import('leaflet');
 
-		const markerIcon = leafletIcon({
-			iconSize: [25, 41],
-			iconAnchor: [10, 41],
-			popupAnchor: [2, -40],
-			iconUrl: markerIconURL,
-			iconRetinaUrl: markerIconRetinaURL,
-			shadowUrl: markerShadowURL
-		});
+	const markerIcon = leafletIcon({
+		iconSize: [25, 41],
+		iconAnchor: [10, 41],
+		popupAnchor: [2, -40],
+		iconUrl: markerIconURL,
+		iconRetinaUrl: markerIconRetinaURL,
+		shadowUrl: markerShadowURL
+	});
 
-		const map = leafletMap(mapElement).setView([latitude, longitude], zoom);
-		tileLayer(
-			'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}',
-			{
-				attribution:
-					'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-				maxZoom: 19,
-				id: 'mapbox/streets-v11',
-				tileSize: 512,
-				zoomOffset: -1,
-				accessToken: mapboxAccessToken,
-				detectRetina: true
-			}
-		).addTo(map);
-
-		if (markerMarkup !== '') {
-			leafletMarker([latitude, longitude], { icon: markerIcon }).bindPopup(markerMarkup).addTo(map);
-		} else {
-			leafletMarker([latitude, longitude], { icon: markerIcon }).addTo(map);
+	const map = leafletMap(mapElement).setView([latitude, longitude], zoom);
+	tileLayer(
+		'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}{r}?access_token={accessToken}',
+		{
+			attribution:
+				'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			maxZoom: 19,
+			id: 'mapbox/streets-v11',
+			tileSize: 512,
+			zoomOffset: -1,
+			accessToken: mapboxAccessToken,
+			detectRetina: true
 		}
-	})();
+	).addTo(map);
+
+	if (markerMarkup !== '') {
+		leafletMarker([latitude, longitude], { icon: markerIcon }).bindPopup(markerMarkup).addTo(map);
+	} else {
+		leafletMarker([latitude, longitude], { icon: markerIcon }).addTo(map);
+	}
+
+	return map;
+}
+
+export function setMap(mapElement: HTMLElement, mapProps: MapNodeProps) {
+	let map: Map | null = null;
+	(async () => (map = await updateMap(mapElement, mapProps)))();
+
+	return {
+		// return update function to be called when props change
+		async update({ latitude, longitude, zoom }: MapNodeProps) {
+			map?.flyTo([latitude, longitude], zoom);
+		}
+	};
 }
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -3,10 +3,24 @@
 	import website from '$lib/config/website';
 
 	const { mapboxAccessToken } = website;
-	const latitude = 51.50162;
-	const longitude = -0.14115;
+
+	const locations = [
+		{ latitude: 51.50162, longitude: -0.14115 }, // Buckingha Palace
+		{ latitude: 51.5007029, longitude: -0.1245872 }, // Big Ben
+		{ latitude: 51.50333, longitude: -0.11969 }, // London Eye
+		{ latitude: 51.5055166, longitude: -0.0753662 }, // Tower Bridge
+		{ latitude: 51.50456, longitude: -0.08658 } // The Shard
+	];
+
+	let index = 0;
+	$: location = locations[index];
+	const locationCount = locations.length;
+
+	function handleNext() {
+		index = (index + 1) % locationCount;
+	}
+
 	const zoom = 16;
-	const location = { latitude, longitude };
 </script>
 
 <svelte:head><title>Map</title></svelte:head>
@@ -20,4 +34,5 @@
 		markerMarkup="<p>We are <strong>here</strong>!</p>"
 		{mapboxAccessToken}
 	/>
+	<button on:click={handleNext}>Fly to next attraction</button>
 </main>
